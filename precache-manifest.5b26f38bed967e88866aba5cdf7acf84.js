56022ffbed9ece2ad7293b13bf5dbf1c43dd2788
Joshua <joshuanwosu078@gmail.com>
2019-06-29 18:55:35 +0100

    Updates

---
+++ precache-manifest.5b26f38bed967e88866aba5cdf7acf84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b8f3df14e9146c89c523c314d6e7f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5d2dee37aa4bf8726d4",
+    "url": "/static/css/2.16aca965.chunk.css"
+  },
+  {
+    "revision": "264c5c4a3f674a2eaa75",
+    "url": "/static/css/main.a129e469.chunk.css"
+  },
+  {
+    "revision": "b5d2dee37aa4bf8726d4",
+    "url": "/static/js/2.2debc43d.chunk.js"
+  },
+  {
+    "revision": "264c5c4a3f674a2eaa75",
+    "url": "/static/js/main.eb120890.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
